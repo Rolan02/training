d4e72e238c90c06aebc4b822facd28ce33071927
Rodrigo Paniagua Camacho <paniagua.camacho.rodrigo@gmail.com>
2023-01-16 20:39:10 -0400

    rodrigo_ejercicio

--- src/Car.java
+++ src/Car.java
@@ -18,7 +18,6 @@ public class Car {
         this.placa = placa;
         this.price = price;
     }
-
     public String getName(){
         return name;
     }
@@ -76,10 +75,21 @@ public class Car {
     public static void findByPrice(List<Car> cars, int price){
         cars.stream().filter(car -> car.getPrice() <= price).forEach(System.out::println);
     }
-    public static void findByModel(List<Car> cars, int model){
-
+    //metodo que filtra los autos segun su modelo
+    public static void findByModel(List<Car> year, int model){
+        year.stream().filter(car -> car.getModel() >= model). forEach(System.out::println);
     }
-    public static void findByEngine(List<Car> cars, int engine){
-
+    //metodo que filtra por numero de motor
+    public static void findByEngine(Car car){
+        System.out.println("======================");
+        System.out.println("El Auto : " + car.getName());
+        System.out.println("motor : " + car.getEngine());
+        System.out.println("======================");
+    }
+    public static void findByColor(Car colors){
+        System.out.println("======================");
+        System.out.println("El Auto : " + colors.getName());
+        System.out.println("color : " + colors.getColor());
+        System.out.println("======================");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
 import java.util.ArrayList;
-import java.util.*;
-
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
         Car toyota = new Car("Toyota",2000,"Blanco","v8",1599, 15000);
         Car mitsubishi = new Car("mitsubishi",2010,"Negro","v6",1598, 20000);
-        Car mazda = new Car("mazda",2020,"ROjo","v6",8598,25000);
+        Car mazda = new Car("mazda",2020,"Rojo","v5",8598,25000);
+        Car BMW = new Car("BMW",2020,"Amarillo","v10",1587, 40000);
+        Car mercedes_benz = new Car("Mercedes_benz",2005,"Celeste","v7",1369, 35000);
+        Car chevrolet = new Car("chevrolet",2023,"verde","v9",7825,30000);
 
         List<Car> cars = new ArrayList<Car>();
         cars.add(toyota);
         cars.add(mitsubishi);
+        cars.add(mazda);
+        cars.add(BMW);
+        cars.add(mercedes_benz);
+        cars.add(chevrolet);
+
+        List<Car> year = new ArrayList<Car>();
+        year.add(toyota);
+        year.add(mitsubishi);
+        year.add(mazda);
+        year.add(BMW);
+        year.add(mercedes_benz);
+        year.add(chevrolet);
+
+        List<Car> engines = new ArrayList<Car>();
+        engines.add(toyota);
+        engines.add(mitsubishi);
+        engines.add(mazda);
+        engines.add(BMW);
+        engines.add(mercedes_benz);
+        engines.add(chevrolet);
 
         //Scanner nos sirve para recuperar datos ingresados desde el teclado del usuario
         Scanner sn = new Scanner(System.in);
@@ -23,7 +47,8 @@ public class Main {
             System.out.println("1.- Buscar por precio");
             System.out.println("2.- Buscar por modelo");
             System.out.println("3.- Buscar por motor");
-            System.out.println("4.- Salir");
+            System.out.println("4.- Buscar por color");
+            System.out.println("5.- Salir");
 
             try {
                 System.out.println("Escribe una de las opciones");
@@ -40,11 +65,35 @@ public class Main {
                         break;
                     case 2:
                         System.out.println("Buscando por modelo........");
+                        int model;
+                        model = sn.nextInt();
+                        System.out.println("========== Vehiculos disponibles por Modelo ========");
+                        Car.findByModel(year,model);
+                        System.out.println("===========================================================");
                         break;
                     case 3:
+                        System.out.println("========== Vehiculos disponibles por Motor ========");
                         System.out.println("Buscando por motor........");
+                        Car.findByEngine(toyota);
+                        Car.findByEngine(mitsubishi);
+                        Car.findByEngine(mazda);
+                        Car.findByEngine(BMW);
+                        Car.findByEngine(mercedes_benz);
+                        Car.findByEngine(toyota);
+                        System.out.println("===========================================================");
                         break;
                     case 4:
+                        System.out.println("========== Vehiculos disponibles por Color ========");
+                        System.out.println("Buscando por color........");
+                        Car.findByColor(toyota);
+                        Car.findByColor(mitsubishi);
+                        Car.findByColor(mazda);
+                        Car.findByColor(BMW);
+                        Car.findByColor(mercedes_benz);
+                        Car.findByColor(toyota);
+                        System.out.println("===========================================================");
+                        break;
+                    case 5:
                         exit = true;
                         break;
                     default:
