fc99f59bedb813187cf5193d8e7aa48c88d62227
Alvin Aldrin Rocha Chavez <virus1sigma@gmail.com>
2023-01-30 21:32:12 -0400

    Se soluciono los problemas causados

--- src/Motocycle.java
+++ src/Motocycle.java
@@ -1,8 +1,8 @@
 public class Motocycle extends VehicleMotor implements IVehicle, IVehicleMotor {
-
-    public Motocycle(String name, String color, int model, int price, String engine, int placa, int pasajer) {
-        super(name, color, model, price, engine, placa,pasajer);
+    public Motocycle(String name, String color, int model, int price, String engine, int pasajer) {
+        super(name, color, model, price, engine, pasajer);
     }
+
     @Override
     public String toString() {
         return "Nombre: " + getName() + "\nModelo: " + getModel() + "\nColor: " + getColor() + "\nMotor: " + getEngine()+ "\nPasajero: " + getPasajer() + "\n";
